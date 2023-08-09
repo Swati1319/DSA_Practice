6638d8bb64af70f9c6e6e04bc9f5e4c2077ebda3
Swati Sharma <79269517+Swati1319@users.noreply.github.com>
2023-08-09 18:19:59 +0530

    Create Largest prime factor.java

---
+++ GFGPOTD/Largest prime factor.java
@@ -0,0 +1,13 @@
+class Solution{
+    static long largestPrimeFactor(int N) 
+    { 
+        long num = 2;
+        // loop till sqrt(N)
+        while ((num * num) <=N)
+            if (N % num == 0) // if num divides N evenly
+                N /= num; // divide N by num
+            else
+                num++; // increase num if it don't divide N
+        return N;
+    }
+}
