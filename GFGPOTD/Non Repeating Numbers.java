e05f1cc4576a486633f9d201209efcea990869ae
Swati Sharma <79269517+Swati1319@users.noreply.github.com>
2023-08-14 10:44:53 +0530

    Create Non Repeating Numbers.java

---
+++ GFGPOTD/Non Repeating Numbers.java
@@ -0,0 +1,34 @@
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        // Code here
+        // Pass 1 : 
+        // Get the XOR of the two numbers we need to find
+        int diff = 0;
+        for(int i: nums)
+        	diff = i ^ diff;
+        // Get its last set bit
+        diff &= -diff;
+        // Pass 2 :
+        int rets[] = {0, 0}; // this vector stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        if(rets[0] > rets[1])
+        {
+            int temp=rets[0];
+            rets[0]=rets[1];
+            rets[1]=temp;
+        }
+        return rets;
+    }
+}
